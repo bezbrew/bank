d31840e95403ee41d39495755d382d767948e54f
Dominik <dominik.krausdk@gmail.com>
2019-12-01 12:29:51 +0100

    Add getter for pesel class

--- src/bank/core/Client.java
+++ src/bank/core/Client.java
@@ -5,10 +5,27 @@ public class Client {
 
     private String customerNumber;
 
+    public Client(String name, String surname, String customerNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.customerNumber = customerNumber;
+
+    }
 
     private String pesel;
 
     public String getPesel() {
         return pesel;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
     }
 }
