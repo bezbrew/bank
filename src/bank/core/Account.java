43d69011537e86c95475c1e1712e1dd461c7c2d9
Dominik <dominik.krausdk@gmail.com>
2019-12-01 11:32:48 +0100

    Account constructors and getters

--- src/bank/core/Account.java
+++ src/bank/core/Account.java
@@ -7,5 +7,27 @@ public class Account {
     private AccountType accountType;
     private BigDecimal balance;
 
+    public Account(String accountNumber, AccountType accountType, BigDecimal balance) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
 
+    public Account(String accountNumber, AccountType accountType) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = BigDecimal.ZERO;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
 }
