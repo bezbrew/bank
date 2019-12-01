088826a7d339e0228b965b6b09123c4894978c75
Dominik <dominik.krausdk@gmail.com>
2019-12-01 11:23:48 +0100

    Create Account class

---
+++ src/bank/core/Account.java
@@ -0,0 +1,11 @@
+package bank.core;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private String accountNumber;
+    private AccountType accountType;
+    private BigDecimal balance;
+
+
+}
